c2bfd6c45a6e2643cdf41a7d6b16e66d582b5227
ImamIfti056 <imamul.ifti@gmail.com>
2025-01-07 00:24:36 +0600

    frontend initialized with react and tailwind

---
+++ frontend/lms/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-blue-800 bg-blue-200 text-center'>Learning Management System</div>
+  )
+}
+
+export default App
