595c1bcbe9668048c400afd80c6edab4d1e10fac
ImamIfti056 <imamul.ifti@gmail.com>
2025-01-07 00:35:04 +0600

    fix: frontend structure

--- frontend/lms/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div className='text-blue-800 bg-blue-200 text-center'>Learning Management System</div>
-  )
-}
-
-export default App
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-blue-200 text-blue-800 text-center'>Learning Management System</div>
+  )
+}
+
+export default App
