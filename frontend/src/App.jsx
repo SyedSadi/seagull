251ff89ed762571fb9e443ce81c59257e24181f4
yuusaif <alvi676@gmail.com>
2025-01-22 22:53:41 +0600

    footer test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
-import Navbar from './components/Shared/Navbar';
-import Footer from './components/Shared/Footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Shared/Navbar";
+import Footer from "./components/Shared/Footer";
 
 const App = () => {
-  return (
-    <>
-      <Navbar/>
-      <Outlet/>
-      <Footer/>
-    </>
+	return (
+		<>
+			<Navbar />
+			<Outlet />
+			<Footer />
+		</>
+	);
+};
 
-  )
-}
-
-export default App
+export default App;
