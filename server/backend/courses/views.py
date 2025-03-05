bcdbce029a0328d627e575726419241de00cf428
ImamIfti056 <imamul.ifti@gmail.com>
2025-03-05 22:02:36 +0600

    solving sonarqube issues

--- server/backend/courses/views.py
+++ server/backend/courses/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import permission_classes
 from .models import Course, CourseContents
 from .serializers import CourseSerializer, CourseContentsSerializer
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
@@ -17,24 +17,56 @@ class CourseDetailView(generics.RetrieveAPIView):  # For retrieving a specific c
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def add_course(request):
-    serializer = CourseSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+# def add_course(request):
+#     serializer = CourseSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['PUT'])
-@permission_classes([IsAuthenticated])
-def update_content(request, id):
-    content = get_object_or_404(CourseContents, id=id)
+# @api_view(['PUT'])
+# @permission_classes([IsAuthenticated])
+# def update_content(request, id):
+#     content = get_object_or_404(CourseContents, id=id)
     
-    serializer = CourseContentsSerializer(content, data=request.data, partial=True)
+#     serializer = CourseContentsSerializer(content, data=request.data, partial=True)
     
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AddCourseView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        """
+        Handle POST request to create a new course.
+        """
+        serializer = CourseSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class UpdateContentView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, id):
+        """
+        Handle PUT request to update existing course content.
+        """
+        content = get_object_or_404(CourseContents, id=id)
+
+        serializer = CourseContentsSerializer(content, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
