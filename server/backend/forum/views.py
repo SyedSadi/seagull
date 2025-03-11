c229a8dfaf11182eea958e27d40461e2b05d1dbe
ImamIfti056 <imamul.ifti@gmail.com>
2025-03-12 03:30:37 +0600

    merged forum, needs testing

--- server/backend/forum/views.py
+++ server/backend/forum/views.py
@@ -7,6 +7,10 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import Post, Comment, Vote
 from .serializers import PostSerializer, CommentSerializer, VoteSerializer
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Vote, Post
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at').select_related('author').prefetch_related('comments__replies')
@@ -22,12 +26,6 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from .models import Vote, Post
-from .serializers import VoteSerializer
 
 class VoteViewSet(viewsets.ModelViewSet):
     queryset = Vote.objects.all()
