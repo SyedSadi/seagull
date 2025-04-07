eee39dab31d642d61b099c014bc530c0dfb67d36
yuusaif <alvi676@gmail.com>
2025-04-08 00:13:58 +0600

    Update server\backend\quiz\views.py

--- server/backend/quiz/views.py
+++ server/backend/quiz/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from .models import Category, Question, Option, QuizAttempt, UserAnswer
 from .serializers import CategorySerializer, QuestionSerializer
