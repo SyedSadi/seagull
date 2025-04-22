ca88e671890d650a30229858a9cb95e5e2a47550
yuusaif <alvi676@gmail.com>
2025-04-23 03:16:53 +0600

    Fix: API doc url

--- server/backend/backend/urls.py
+++ server/backend/backend/urls.py
@@ -44,6 +44,5 @@ urlpatterns = [
 
     #API Documentation
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
