7cd2666daa23894875383b145231b17a38756da9
ImamIfti056 <imamul.ifti@gmail.com>
2025-04-30 11:32:33 +0600

    update: cleaned code, new: project story

--- server/backend/devlog/tests/test_views.py
+++ server/backend/devlog/tests/test_views.py
@@ -7,11 +7,3 @@ def test_home_page_renders_correct_template(client):
     response = client.get(reverse('home'))  # Make sure you have the correct URL pattern name
     assert response.status_code == 200
     assert 'devlog/home.html' in [t.name for t in response.templates]
-
-
-@pytest.mark.django_db
-def test_about_page_renders_correct_template(client):
-    """Test that the story page renders the correct template."""
-    response = client.get(reverse('story'))  # Make sure you have the correct URL pattern name
-    assert response.status_code == 200
-    assert 'devlog/story.html' in [t.name for t in response.templates]
